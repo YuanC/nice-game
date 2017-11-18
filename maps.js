71a144f59e632628b0bfeb381c96bb1dc414be81
Justin Koh <jkoh8@uwo.ca>
2017-11-18 01:35:42 -0500

    try

--- maps.js
+++ maps.js
@@ -8,6 +8,9 @@ exp.updateWeather = (places) => {
   console.log('updating map')
 
   // fetches weather from API for each location in places
+  // for(let location in places){
+  $.get("https://hackathon.pic.pelmorex.com/api/data/ssp?locationcode=CAON0383",json)
+  // }
   // if periods && periods[0]['intensity'] != "0" {
     // set place.precip to true
   // else false
